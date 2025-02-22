cc417281d51ba207057b2ae1c67aaf6f0fcec10e
AnnaBilokon <Anna.bilokon89@gmail.com>
2025-02-22 17:51:22 +0100

    add filter and fetching logic

--- components/companies/CompaniesContainer.tsx
+++ components/companies/CompaniesContainer.tsx
@@ -1,31 +1,104 @@
+"use client";
+import { useState, useEffect } from "react";
 import CompaniesList from "./CompaniesList";
 import { Separator } from "@/components/ui/separator";
 import { companies } from "@/lib/data";
+import FilterBar from "./FilterBar";
+import { supabase } from "@/lib/supabaseClient";
 
-// {}: { layout: string; search: string }
-async function CompaniesContainer() {
-  const totalCompanies = companies.length;
-  //   const searchTerm = search ? `&search=${search}` : "";
+function CompaniesContainer() {
+  const [filteredCompanies, setFilteredCompanies] = useState(companies);
+  const [companyData, setCompanyData] = useState(companies);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchCompanies = async () => {
+      try {
+        setLoading(true);
+        const { data, error } = await supabase.from("companies").select("*");
+        console.log("Supabase Error: ", error);
+
+        if (error) {
+          throw error;
+        }
+
+        setCompanyData((prevData) => [...prevData, ...(data || [])]);
+      } catch (err) {
+        setError("Error fetching companies");
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCompanies();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  const uniqueCompanies = companyData.filter(
+    (value, index, self) =>
+      index === self.findIndex((t) => t.name === value.name)
+  );
+
+  const handleFilter = (filterType: string, value: string | null) => {
+    if (filterType === "reset" || !value) {
+      setFilteredCompanies(uniqueCompanies);
+      return;
+    }
+
+    let updatedCompanies = [...uniqueCompanies];
+
+    if (filterType === "price") {
+      updatedCompanies.sort((a, b) =>
+        value === "low-to-high" ? a.price - b.price : b.price - a.price
+      );
+    }
+
+    if (filterType === "industry") {
+      updatedCompanies = uniqueCompanies.filter(
+        (company) => company.industry === value
+      );
+    }
+
+    if (filterType === "keyword" && value) {
+      updatedCompanies = uniqueCompanies.filter(
+        (company) =>
+          company.name.toLowerCase().includes(value.toLowerCase()) ||
+          company.description.toLowerCase().includes(value.toLowerCase())
+      );
+    }
+
+    setFilteredCompanies(updatedCompanies);
+    console.log(filterType, value, updatedCompanies);
+  };
+
   return (
     <>
       <section className="mt-32">
-        <div className="flex justify-between items-center">
-          <h4 className="font-medium text-lg">
-            {totalCompanies} product{totalCompanies > 1 && "s"}
-          </h4>
-        </div>
         <Separator className="mt-4" />
+
+        <FilterBar onFilter={handleFilter} />
+        <div className="flex justify-between items-center mt-4"></div>
+        <div>
+          {filteredCompanies.length === 0 ? (
+            <h5 className="text-2xl mt-16">
+              Sorry, no products matched your search...
+            </h5>
+          ) : (
+            <CompaniesList companies={filteredCompanies} />
+          )}
+        </div>
       </section>
-      <div>
-        {totalCompanies === 0 ? (
-          <h5 className="text-2xl mt-16">
-            Sorry, no products matched your search...
-          </h5>
-        ) : (
-          <CompaniesList companies={companies} />
-        )}
-      </div>
     </>
   );
 }
+
 export default CompaniesContainer;
