25be98f5fda2108acde390c49767d25ddcd54c20
AnnaBilokon <Anna.bilokon89@gmail.com>
2025-02-23 19:04:07 +0100

    create the footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,24 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-900 text-white py-6 mt-10">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-6">
+        <p className="text-sm">
+          &copy; {new Date().getFullYear()} MARKET PLACE. All rights reserved.
+        </p>
+        <div className="flex space-x-4 mt-4 md:mt-0">
+          <a href="#" className="hover:underline">
+            Privacy Policy
+          </a>
+          <a href="#" className="hover:underline">
+            Terms of Service
+          </a>
+          <a href="#" className="hover:underline">
+            Contact
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
