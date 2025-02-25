2d54d857470af6e561397f177a54fa86cfb5b8ad
AnnaBilokon <Anna.bilokon89@gmail.com>
2025-02-25 09:46:43 +0100

    create the seller dashboard to display interested buyers

---
+++ app/seller-dashboard/page.tsx
@@ -0,0 +1,12 @@
+import InterestedBuyersList from "@/components/InterestedBuyersList";
+
+const SellerDashboard = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-bold">Seller Dashboard</h1>
+      <InterestedBuyersList />
+    </div>
+  );
+};
+
+export default SellerDashboard;
